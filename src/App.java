fa4dc5ee0bb6ddcc0cf02e892f9d241c71469f4a
Tim <tim.englund@elev.ga.ntig.se>
2023-09-25 09:03:55 +0200

    Lade till fält2 samt info om bägge fält

--- src/App.java
+++ src/App.java
@@ -19,5 +19,19 @@ public class App {
         double medelvärde = (fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4])/5;
         System.out.println("Medelvärdet är: "+medelvärde);
 
+        int[] fält2 = new int[5];
+        fält2[0] = 18;
+        fält2[1] = 14;
+        fält2[2] = 16;
+        fält2[3] = 13;
+        fält2[4] = 19;
+
+        int summa_f1 = (fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4]);
+        System.out.println("Summan av fält1 = "+summa_f1);
+        int summa_f2 = (fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4]);
+        System.out.println("Summan av fält2 = "+summa_f2);
+        double med_Ftot = (summa_f1+summa_f2)/2;
+        System.out.println("Medelvärdet av fält1+2 = "+med_Ftot);
+
     }
 }
