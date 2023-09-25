34a968282967a9bc5604870ce27ba26a6756ea4e
Tim <tim.englund@elev.ga.ntig.se>
2023-09-25 08:55:01 +0200

    Lade till array samt lite info om dessa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        int[] fält1 = new int[5];
+        fält1[0] = 8;
+        fält1[1] = 4;
+        fält1[2] = 6;
+        fält1[3] = 3;
+        fält1[4] = 9;
+
+        System.out.println("Element 0: "+fält1[0]);
+        System.out.println("Element 1: "+fält1[1]);
+        System.out.println("Element 2: "+fält1[2]);
+        System.out.println("Element 3: "+fält1[3]);
+        System.out.println("Element 4: "+fält1[4]);
+
+        System.out.println("Antal element i fält1: "+fält1.length);
+    
+        double medelvärde = (fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4])/5;
+        System.out.println("Medelvärdet är: "+medelvärde);
+
     }
 }
